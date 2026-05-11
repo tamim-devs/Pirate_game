2e25c97937405e7d64fb00934ba9190dc0cc3806
tamim-devs <tamimh0727@gmail.com>
2026-05-11 19:40:55 +0600

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
 import Image from "next/image";
 import Link from "next/link";
 import {Avatar, Button} from "@heroui/react";
 const Navbar = () => {
-
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+  console.log(user,"user data in navbar")
   const handleSignOut = async () => {
-  
+    await authClient.signOut();
   }
   return (
     <div className="border-b px-2">
@@ -15,8 +18,8 @@ const Navbar = () => {
             src={"/logo.png"}
             alt="logo"
             loading="eager"
-            width={50}
-            height={50}
+            width={30}
+            height={30}
             className="object-cover h-auto w-auto"
           />
           <h3 className="font-black text-lg">Pirate Game</h3>
@@ -27,16 +30,19 @@ const Navbar = () => {
             <Link href={"/"}>Home</Link>
           </li>
           <li>
-            <Link href={"/products"}>All Products</Link>
+            <Link href={"/all-photos"}>All Photos</Link>
           </li>
-          
+          <li>
+            <Link href={"/pricing"}>Pricing</Link>
+          </li>
           <li>
             <Link href={"/profile"}>Profile</Link>
           </li>
         </ul>
 
         <div className="flex gap-4">
-           <ul className="flex items-center  gap-4 text-sm">
+    {!user && (
+  <ul className="flex items-center  gap-4 text-sm">
             <li>
               <Link className="hover:underline" href={"/signup"}>SignUp</Link>
             </li>
@@ -44,6 +50,20 @@ const Navbar = () => {
               <Link className="hover:underline" href={"/signin"}>SignIn</Link>
             </li>
           </ul>
+    )}
+
+    {user && (
+      <div className="flex items-center gap-4">
+        <Avatar>
+        <Avatar.Image alt="John Doe" src={user?.image}
+        referrerPolicy="no-referrer" />
+        <Avatar.Fallback>{user?.name?.charAt(0)}</Avatar.Fallback>
+      </Avatar>
+      <Button onClick={handleSignOut} variant="tertiary">
+        Sign Out
+      </Button>
+      </div>
+    )}
         
         </div>
       </nav>
